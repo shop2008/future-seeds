c3954196e13cf0704da5699065808fc5d58bf182
Gigi <gigiwangjob@gmail.com>
2025-03-19 09:40:40 -0500

    feat: add PWA support with service worker and manifest file

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "future-seeds-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/about.html",
+  "/contact.html",
+  "/donate.html",
+  "/get-involved.html",
+  "/projects.html",
+  "/volunteer-signup.html",
+  "/css/styles.css",
+  "/js/main.js",
+  "/manifest.json",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).then((response) => {
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response;
+        }
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+        return response;
+      });
+    })
+  );
+});
